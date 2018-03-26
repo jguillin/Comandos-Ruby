4189ae9d2ff01e3403d847ee063e7abc40714720
jguillin <jguillin@espol.edu.ec>
2018-03-26 00:35:36 -0500

    B1

---
+++ ComandosRuby.rb
@@ -0,0 +1,7 @@
+puts "Wow!!!!!!!!!1" #Imprime y realiza un salto de línea automático.
+print "Comandos para Ruby\n" #Sólo imprime sin realizar salto de línea.
+array = ["Ruby", "Python", "Scala", "Java"] #Lista
+puts array.sort #Ordena una Lista
+puts array #Presenta los elementos de la lista
+( array = array.sort ) == array.sort!
+puts “Ruby” + “ on Rails” #El + concatea caracteres
